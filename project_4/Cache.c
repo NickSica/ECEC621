9d7dc7a3f5b202c3cdf46c243c870a43f8a567f6
cjawesomest <cjcalv@gmail.com>
2019-11-20 21:01:08 -0500

    Fixed SegFaulting

--- project_4/Cache.c
+++ project_4/Cache.c
@@ -3,9 +3,9 @@
 /* Constants */
 const unsigned block_size = 64; // Size of a cache line (in Bytes)
 // TODO, you should try different size of cache, for example, 128KB, 256KB, 512KB, 1MB, 2MB
-const unsigned cache_size = 128; // Size of a cache (in KB)
+const unsigned cache_size = 2048; // Size of a cache (in KB)
 // TODO, you should try different association configurations, for example 4, 8, 16
-const unsigned assoc = 4;
+const unsigned assoc = 8;
 
 Cache *initCache()
 {
@@ -101,10 +101,6 @@ bool accessBlock(Cache *cache, Request *req, uint64_t access_time)
     {
 	blk->outcome = true;
 	shct[blk->signature_m]++;
-    }
-    else
-    {
-	blk->outcome = false;
     }
     
     return hit;
